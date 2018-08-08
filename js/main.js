fc782112c25d8c29c9aa64f2a508f0caf3a7f63a
Herisson C R Silva <herisson.cleiton.r@gmail.com>
2018-08-08 00:21:07 -0300

    Portifólio 1.0_RT

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var UID = {
+    _current: 0,
+    getNew: function () {
+        this._current++;
+        return this._current;
+    }
+};
+
+HTMLElement.prototype.pseudoStyle = function (element, prop, value) {
+    var _this = this;
+    var _sheetId = "pseudoStyles";
+    var _head = document.head || document.getElementsByTagName('head')[0];
+    var _sheet = document.getElementById(_sheetId) || document.createElement('style');
+    _sheet.id = _sheetId;
+    var className = "pseudoStyle" + UID.getNew();
+
+    _this.className += " " + className;
+
+    _sheet.innerHTML += " ." + className + ":" + element + "{" + prop + ":" + value + "}";
+    _head.appendChild(_sheet);
+    return this;
+};
